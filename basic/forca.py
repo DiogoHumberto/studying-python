b010100e776ccc247d76fadfd7e495b5d1a3b572
Diogo Humberto <diogo_humberto@hotmail.com>
2022-06-29 16:31:12 -0300

    implementando jogo forca

--- basic/forca.py
+++ basic/forca.py
@@ -1,6 +1,22 @@
 
 def jogar():
-    print("*********JOGO EM CONSTRUÇÃO*********")
+    print("*********Bem vido ao jogo da Forca!*********")
+
+    palavra_secreta = "pyhton"
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+
+        chute = input("Qual a letra ? ").strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Enontrado a letra {} na posição {}".format(chute, index))
+            index = index + 1
+        print("Jogando!!")
 
 if(__name__ == "__main__"):
     jogar()
