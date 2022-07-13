b8ba47de7f4eaaa61ce283246e84c8649cf28e5c
Diogo Humberto <diogo_humberto@hotmail.com>
2022-07-13 12:34:53 -0300

    organizando codigo e metodos

--- basic/forca.py
+++ basic/forca.py
@@ -2,39 +2,29 @@ import random
 
 
 def jogar():
-    exibirMsgIncioJogo()
-    arquivo = open('palavras.txt', 'r')
-    palavras = [linha.strip() for linha in arquivo]
-    arquivo.close()
-
-    palavra_secreta = palavras[random.randrange(0, len(palavras))].upper()
-
-    letras_acertadas = ["_" for _ in palavra_secreta]
+    exibirMsgIncio()
+    palavraSecreta = carregaPalavraSecreta()
+    letrasAcertadas = inciaLayoutPalavraAcertadas(palavraSecreta)
+    print(letrasAcertadas)
 
     enforcou = False
     acertou = False
     erros = 0
 
-    print(letras_acertadas)
-
     while not enforcou and not acertou:
 
-        chute = input("Qual a letra ? ").strip().upper()
+        chute = inputLetraChute()
 
-        if chute in palavra_secreta:
-            index = 0
-            for letra in palavra_secreta:
-                if chute == letra:
-                    letras_acertadas[index] = letra
-                index += 1
+        if chute in palavraSecreta:
+            letrasAcertadas = preencheLetrasAcertadas(palavraSecreta, letrasAcertadas, chute)
 
         else:
             erros += 1
             print("Letra não encontrada!")
         enforcou = erros == 6
-        acertou = "_" not in letras_acertadas
+        acertou = "_" not in letrasAcertadas
 
-        print(letras_acertadas)
+        print(letrasAcertadas)
 
     if acertou:
         print("Você ganhou!!!")
@@ -44,10 +34,35 @@ def jogar():
     print("FIM DE JOGO")
 
 
-def exibirMsgIncioJogo():
+def exibirMsgIncio():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
+
+
+def carregaPalavraSecreta():
+    arquivo = open('palavras.txt', 'r')
+    palavras = [linha.strip() for linha in arquivo]
+    arquivo.close()
+    return palavras[random.randrange(0, len(palavras))].upper()
+
+
+def inciaLayoutPalavraAcertadas(palavras):
+    return ["_" for _ in palavras]
+
+
+def inputLetraChute():
+    chute = input("Qual a letra contem a palavra? ").strip().upper()
+    while not len(chute) == 1:
+        chute = input("Preencha somente uma letra! Qual a letra contem a palavra? ").strip().upper()
+    return chute
+
+
+def preencheLetrasAcertadas(palavraSecreta, letrasAcertadas, chute):
+    for index, letra in enumerate(palavraSecreta):
+        if chute == letra:
+            letrasAcertadas[index] = letra
+    return letrasAcertadas
 
 
 if __name__ == "__main__":
