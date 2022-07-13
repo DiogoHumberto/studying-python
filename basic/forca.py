286781df9fe599f3e96191725ce8cf2b0b074c27
Diogo Humberto <diogo_humberto@hotmail.com>
2022-07-13 14:01:33 -0300

    adicionando msg personalizada

--- basic/forca.py
+++ basic/forca.py
@@ -20,18 +20,16 @@ def jogar():
 
         else:
             erros += 1
-            print("Letra não encontrada!")
-        enforcou = erros == 6
+            desenha_forca(erros)
+        enforcou = erros == 7
         acertou = "_" not in letrasAcertadas
 
         print(letrasAcertadas)
 
     if acertou:
-        print("Você ganhou!!!")
+        exibeMsgVencedor()
     else:
-        print("Você perdeu!!!")
-
-    print("FIM DE JOGO")
+        exibeMsgPerdedor(palavraSecreta)
 
 
 def exibirMsgIncio():
@@ -63,6 +61,87 @@ def preencheLetrasAcertadas(palavraSecreta, letrasAcertadas, chute):
         if chute == letra:
             letrasAcertadas[index] = letra
     return letrasAcertadas
+
+
+def exibeMsgPerdedor(palavra_secreta):
+    print("Puxa, você foi enforcado!")
+    print("A palavra era {}".format(palavra_secreta))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+
+def exibeMsgVencedor():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    match erros:
+        case 1:
+            print(" |      (_)   ")
+            print(" |            ")
+            print(" |            ")
+            print(" |            ")
+        case 2:
+            print(" |      (_)   ")
+            print(" |      \     ")
+            print(" |            ")
+            print(" |            ")
+        case 3:
+            print(" |      (_)   ")
+            print(" |      \|    ")
+            print(" |            ")
+            print(" |            ")
+        case 4:
+            print(" |      (_)   ")
+            print(" |      \|/   ")
+            print(" |            ")
+            print(" |            ")
+        case 5:
+            print(" |      (_)   ")
+            print(" |      \|/   ")
+            print(" |       |    ")
+            print(" |            ")
+        case 6:
+            print(" |      (_)   ")
+            print(" |      \|/   ")
+            print(" |       |    ")
+            print(" |      /     ")
+        case 7:
+            print(" |      (_)   ")
+            print(" |      \|/   ")
+            print(" |       |    ")
+            print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
 
 
 if __name__ == "__main__":
